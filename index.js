c12632756c1fd52e301176ad787bfa00082d9f54
王鸿兴 <851094531@qq.com>
2015-06-22 17:59:58 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var through = require("through2");
 var path = require("path");
 var fs = require("fs");
-var URL_REG = /(?:"|'|\()(?:\/?([\w.-]|\.|\.\.)*\/)*?[\w-]*?\.(?:png|jpg|gif|bmp)(?:\?[\w=]*)?(?:"|'|\))/g;
+var URL_REG = /("|'|\()((?:\/?(?:[\w.-]|\.|\.\.)*\/)*?[\w-]*?\.(png|jpg|gif|bmp))(?:\?[\w=]*)?("|'|\))/g;
 
 module.exports = function (options) {
     var opt = {
@@ -10,12 +10,13 @@ module.exports = function (options) {
         pathrep: null  //将路由地址的图片路径替换成相对地址,格式为{reg:'',rep:''}
     };
 
+    options = (typeof options === "object") ? options : {};
     for (var k in options) {
         if (!(k in opt)) continue;
         opt[k] = options[k];
     }
 
-    var imgPath, i, ref;
+    var i, ref;
     var ignore = opt.ignore,
         maxsize = typeof +opt.maxsize === "number" ? +opt.maxsize : 1,
         pathrep = typeof opt.pathrep === "object" ? opt.pathrep : null;
@@ -23,58 +24,59 @@ module.exports = function (options) {
     var _transform = function (file, encoding, done) {
         var str = String(file.contents);
         var p = file.path.substring(0, file.path.lastIndexOf("\\"));
-
+        var count = 0;
         str = str.replace(URL_REG, function (m) {
+            var prefix = RegExp.$1;       //$1为匹配的开头的"或'或(
+            var suffix = RegExp.$4;       //$4为匹配的结尾的"或'或)
+            var imgkind = RegExp.$3;      //$3为匹配的图片类型
+            var imgPath = RegExp.$2;      //$2为路径内容
+
             //判断ignore的值并进行相应处理
-            if(ignore){
+            if (ignore) {
                 if (typeof ignore === "string") {
                     if (m.indexOf(ignore) >= 0) return m
                 } else if ('splice' in ignore) {
                     ref = false;
                     for (i = 0; i < ignore.length; i++) {
-                        if(typeof ignore[i] === "string" && m.indexOf(ignore[i]) >= 0){
+                        if (typeof ignore[i] === "string" && m.indexOf(ignore[i]) >= 0) {
                             ref = true;
                             break;
                         }
                     }
-                    if(ref) return m;
-                }else if(ignore instanceof RegExp){
-                    if(m.match(ignore)) return m;
+                    if (ref) return m;
+                } else if (ignore instanceof RegExp) {
+                    if (m.match(ignore)) return m;
                 }
             }
-
-            imgPath = m;
-            var q = imgPath.charAt(0);      //获取首字符
-            var h = imgPath.charAt(imgPath.length - 1);   //获取尾字符
-            imgPath = imgPath.substring(1, imgPath.length - 1);      //获取路径内容
-            var fk = imgPath.substring(imgPath.lastIndexOf(".") + 1, imgPath.length);        //获取图片后缀名
 
             //如果是路由地址，则替换为相对地址，否则根据文件位置匹配出相应的绝对地址
-            if(pathrep && (pathrep.reg instanceof RegExp) && m.match(pathrep.reg)){
-                imgPath = imgPath.replace(pathrep.reg , pathrep.rep || "");
-            }else {
+            if (pathrep && (pathrep.reg instanceof RegExp) && m.match(pathrep.reg)) {
+                imgPath = imgPath.replace(pathrep.reg, pathrep.rep || "");
+            } else {
                 imgPath = path.resolve(p, imgPath);
             }
-            try {
-                var stats = fs.statSync(imgPath);
-                //如果图片文件小于1kb则替换为base64格式
-                if (stats.size / 1024 < maxsize) {
-                    var baseStr = fs.readFileSync(imgPath).toString("base64");
-                    imgPath = q + "data:image/" + fk + ";base64," + baseStr + h;
-                    //console.log(file.path)
-                    //console.log(m +" ==> [length:"+imgPath.length+"]" + imgPath.substring(0 , 50) + "..." + imgPath.substring(imgPath.length-10 , imgPath.length))
-                    return imgPath;
-                }
-            } catch (e) {
-                //console.log(e)
+
+            if (!fs.existsSync(imgPath)) {
+                console.log("\x1B[31mFile is not found:" + imgPath + "\x1B[0m");
+                return m;
+            }
+
+            //如果图片文件小于1kb则替换为base64格式
+            if ((fs.statSync(imgPath).size / 1024) < maxsize) {
+                var baseStr = fs.readFileSync(imgPath).toString("base64");
+                imgPath = prefix + "data:image/" + imgkind + ";base64," + baseStr + suffix;
+                count++;
+                return imgPath;
             }
 
             return m;
         });
 
+        if (count)console.log("\x1B[32m" + file.path + " has " + count + " change\x1B[0m");
+
         file.contents = new Buffer(str);
 
-        done(null , file);
+        done(null, file);
     };
 
     return through.obj(_transform)
